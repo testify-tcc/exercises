0a0d215841c09deb7d3a1b821a327671ed36c7d7
Vinicius Pereira <vini.gpereira2014@gmail.com>
2022-09-24 16:53:59 -0300

    chore: update environment variables

--- env.py
+++ env.py
@@ -3,4 +3,5 @@ import os
 CLIENT_DEV_ENDPOINT = "http://localhost:8080"
 CLIENT_PROD_ENDPOINT = "http://testify.surge.sh"
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-EXERCISES_PATH = f"{ROOT_PATH}/exercises"
+SECTIONS_PATH = f"{ROOT_PATH}/definitions/sections"
+EXERCISES_PATH = f"{ROOT_PATH}/definitions/exercises"
