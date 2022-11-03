e8de085d09be8e3131e475a2e06d28235143626c
Vinicius Pereira <vini.gpereira2014@gmail.com>
2022-11-03 18:21:23 -0300

    fix: no extra editing section in roman numerals's exercise test file

--- definitions/exercises/roman-numerals/files/roman-numerals.test.ts
+++ definitions/exercises/roman-numerals/files/roman-numerals.test.ts
@@ -3,6 +3,9 @@
 import { convertRomanNumeral } from './roman-numerals';
 
 describe("convertRomanNumeral", () => {
+  // BEGINNING EXTRA EDITING SECTION
+  // ENDING EXTRA EDITING SECTION
+
   // BEGINNING EDITING SECTION
   it("", () => {});
   // ENDING EDITING SECTION
